9afe0fbbd959587d6976a01380d6a5903b895818
Italkebby <roberto.cannone96@gmail.com>
2021-10-16 14:57:24 +0200

    aggiunti nuovi file installazione

--- install.sh
+++ install.sh
@@ -1,77 +1,25 @@
-echo setto il tasto per il shutdown
+echo "INSTALLL ALLL YOU CAN NEED"
+
+touch log.txt
+echo "let's go" >> log.txt
+
+
+
+
+#after login ipscan wont spam on serial
+echo "pkill -f ./script/ipspam" > /etc/profile
+
+bash ./script/be4_reboot.txt
+echo "WARNING: make sure you are connected to internet"
+read
+
 sudo apt update 
 sudo apt install aptitude -y
-sudo aptitude install acpid -y
-cd /etc/acpi/
-touch shutdown.sh
-
-echo "event=button/power" | sudo tee -a shutdown.sh > /dev/null
-echo "action=/sbin/shutdown -h now" | sudo tee -a shutdown.sh > /dev/null
-sudo chmod +x shutdown.sh
-cp shutdown.sh events/
-cd events
-mv shutdown.sh button_power
-sudo /etc/init.d/acpid restart
 
 
-echo "fixo le gpio"
-sudo apt install git --upgrade
-git -c http.sslVerify=false clone http://wiki.pbeirne.com/patb/i96 gpio_fix
-git -c http.sslVerify=false clone https://github.com/radii/devmem2.git
-cd devmem2
-gcc devmem2.c -o devmem2
-cp devmem2 /usr/bin/
-cd 
+bash ./script/power_button_install.txt
+bash ./script/fixing_gpio.txt
 
-
-
-bash gpio_fix/gpio_fixup.sh
-
-
-
-da fare 
-
-echo "creo startup service"
-
-sudo nano /etc/systemd/system/gpio_fixup.service
-
-nano /etc/init.d/gpio_fixup.conf
-chmod u+x 
-[Unit]
-Description=Fix for gpio
-
-[Service]
-ExecStart=/usr/local/bin/i96/gpio_fixup.py
-
-[Install]
-WantedBy=multi-user.target
-
-
-
-echo "api gpio -> python OPIO"
-cd
-git -c http.sslVerify=false clone http://wiki.pbeirne.com/patb/opio/
-
-./opio write 101 on -d
-./opio write 125 on -d
-./opio write 126 on -d
-
-./opio write 101 off /
-./opio write 125 off /
-./opio write 126 off
-
-(d per dire low level)
-
-
-sudo apt install software-properties-common -y
-sudo apt install dirmngr --install-recommends -y
-sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt update
-
-sudo apt-get install i2c-tools -y
-i2cdetect -y 0 
-i2cdetect -y 1 
-i2cdetect -y 2 
 
 
 
