ce0a32438f00a80167b47f102416ef2640b8fcbc
Italkebby <roberto.cannone96@gmail.com>
2021-10-16 17:25:58 +0200

    update0.2 install.sh

--- install.sh
+++ install.sh
@@ -1,23 +1,26 @@
 echo "INSTALLL ALLL YOU CAN NEED"
-
 touch log.txt
 echo "let's go" >> log.txt
 
-
-
-
-#after login ipscan wont spam on serial
-echo "pkill -f ./script/ipspam" > /etc/profile
-
-bash ./script/be4_reboot.txt
 echo "WARNING: make sure you are connected to internet"
 read
+bash $(pwd)/script/be4_reboot.txt
 
+echo "STATUS: IP spam in serial in startup and after login will be removed"
+echo "$pwd/script/ipspam.txt & DA FIXARE" > log.txt
+
+#after login from LAN ipscan won't spam on serial
+echo "pkill -f $pwd/script/ipspam.txt" > /etc/profile
+
+
+echo "STATUS: Installing pre-requirements"
 sudo apt update 
 sudo apt install aptitude -y
 
+echo "aptitude installed">>log.txt
 
 bash ./script/power_button_install.txt
+
 bash ./script/fixing_gpio.txt
 
 
